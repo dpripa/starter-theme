bbf75c01576305f1ce0a2c390330fd36eb01f7cd
Dmitry Pripa <dmitrypripa@gmail.com>
2024-07-14 20:12:10 +0300

    Update block-example.php

--- acf-block/block-example.php
+++ acf-block/block-example.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Block Name:        Block Example
+ * Block Description: A block example
+ * Block Icon:        slides
+ * Block Keywords:    example
+ * Block Post Types:  post, page
+ */
+
 namespace MyTheme;
 
 defined( 'ABSPATH' ) || exit;
+?>
+<div class="my-block"></div>
