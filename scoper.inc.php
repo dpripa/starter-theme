317f35d1e9ae456f9ecf2927cbe64591327b6585
Dmitry Pripa <dmitrypripa@gmail.com>
2025-08-16 00:55:28 +0300

    Add ACF Builder dependency and update scoper config
    Added 'stoutlogic/acf-builder' to composer.json and updated composer.lock accordingly. Updated scoper.inc.php to include ACF Builder files for PHP-Scoper processing. Removed unused .gitkeep from acf-json directory.

--- scoper.inc.php
+++ scoper.inc.php
@@ -19,6 +19,13 @@ return array(
 			->in( 'vendor/omgpress/acf-block-autoloader' )
 			->append( array( 'vendor/omgpress/acf-block-autoloader/composer.json' ) ),
 		Finder::create()
+			->files()
+			->ignoreVCS( true )
+			->name( '*.php' )
+			->notName( '/LICENSE|.*\\.md|.*\\.dist|Makefile|composer\\.json|composer\\.lock/' )
+			->in( 'vendor/stoutlogic/acf-builder' )
+			->append( array( 'vendor/stoutlogic/acf-builder/composer.json' ) ),
+		Finder::create()
 			->append( array( 'composer.json' ) ),
 	),
 	'exclude-classes' => require_once __DIR__ . '/vendor/omgpress/omgcore/scoper.exclude-classes.php',
